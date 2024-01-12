e226cc2c0fcf3775ace4aadcdb83c1384a8d89c8
ReturnerNaGithub <damsan1@gmail.com>
2024-01-12 01:39:53 +0100

    drugi commit

---
+++ app/page.tsx
@@ -0,0 +1,52 @@
+import { PrismaClient } from "@prisma/client";
+
+import AddTodo from "@/components/shared/AddTodo";
+
+
+import Todo from "@/components/shared/Todo";
+
+const prisma = new PrismaClient();
+async function getData() {
+  const data = await prisma.todo.findMany({
+    select: {
+      title: true,
+      id: true,
+      isCompleted: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return data;
+}
+const Home = async () => {
+  const data = await getData();
+  return (
+    <div className="w-screen py-20 flex justify-center flex-col items-center">
+      <span className="text-3xl font-extrabold text-purple-100 ml-2">
+        Our Financial App
+      </span>
+      <h1 className=" text-3xl  mb-5 text-purple-100 ml-2">
+      Goal:
+        <span className="text-purple-100 ml-2">
+          980.000 EUR
+        </span>
+      </h1>
+
+      <div className="flex justify-center flex-col items-center w-[1000px] ">
+        <AddTodo />
+        <div className=" flex flex-col gap-5 items-center justify-center mt-10 w-full ">
+          {data.map((todo, id) => (
+            <div className="w-full " key={id}>
+              <Todo todo={todo} />
+            </div>
+          ))}
+        </div>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Home;
